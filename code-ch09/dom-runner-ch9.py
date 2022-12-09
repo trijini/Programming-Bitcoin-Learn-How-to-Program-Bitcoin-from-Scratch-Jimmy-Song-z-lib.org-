4ec0decba583518bd5bd3a72e42b630fe39f61ff
Daniel Olaya <daniel.olayam@gmail.com>
2022-12-08 21:17:06 -0800

    wip ch.9

---
+++ code-ch09/dom-runner-ch9.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from importlib import reload
+from helper import run
+import block
+import ecc
+import helper
+import script
+import tx
+
+
+reload(tx)
+run(tx.TxTest("test_is_coinbase"))
--- code-ch09/tx.py
+++ code-ch09/tx.py
@@ -22,14 +22,14 @@ class TxFetcher:
     @classmethod
     def get_url(cls, testnet=False):
         if testnet:
-            return 'http://testnet.programmingbitcoin.com'
+            return 'https://blockstream.info/testnet/api'
         else:
-            return 'http://mainnet.programmingbitcoin.com'
+            return 'https://blockstream.info/api'
 
     @classmethod
     def fetch(cls, tx_id, testnet=False, fresh=False):
         if fresh or (tx_id not in cls.cache):
-            url = '{}/tx/{}.hex'.format(cls.get_url(testnet), tx_id)
+            url = '{}/tx/{}/hex'.format(cls.get_url(testnet), tx_id)
             response = requests.get(url)
             try:
                 raw = bytes.fromhex(response.text.strip())
@@ -258,10 +258,18 @@ class Tx:
     def is_coinbase(self):
         '''Returns whether this transaction is a coinbase transaction or not'''
         # check that there is exactly 1 input
+        if len(self.tx_ins) != 1:
+            return False
         # grab the first input
+        coinbase = self.tx_ins[0]
+        template = b'\x00' * 32
         # check that first input prev_tx is b'\x00' * 32 bytes
+        if coinbase.prev_tx != template:
+            return False
         # check that first input prev_index is 0xffffffff
-        raise NotImplementedError
+        if coinbase.prev_index != 0xffffffff:
+            return False
+        return True
 
     def coinbase_height(self):
         '''Returns the height of the block this coinbase transaction is in
