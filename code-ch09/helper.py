4f4048410d11e02652989a48fb43d1f09059ae0d
Daniel Olaya <daniel.olayam@gmail.com>
2022-12-14 10:27:20 -0800

    ch9 completed

--- code-ch09/helper.py
+++ code-ch09/helper.py
@@ -155,11 +155,16 @@ def calculate_new_bits(previous_bits, time_differential):
     '''Calculates the new bits given
     a 2016-block time differential and the previous bits'''
     # if the time differential is greater than 8 weeks, set to 8 weeks
-    # if the time differential is less than half a week, set to half a week
+    if time_differential > TWO_WEEKS * 4:
+        time_differential = TWO_WEEKS * 4
+    # if the differential < 1/2 week, set to 1/2 week
+    if time_differential < TWO_WEEKS // 4:
+        time_differential = TWO_WEEKS // 4
     # the new target is the previous target * time differential / two weeks
+    new_target = bits_to_target(previous_bits) * time_differential // TWO_WEEKS
     # if the new target is bigger than MAX_TARGET, set to MAX_TARGET
     # convert the new target to bits
-    raise NotImplementedError
+    return target_to_bits(new_target)
 
 
 class HelperTest(TestCase):
