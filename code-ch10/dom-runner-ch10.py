5a18df191dadfeb1287ab362ba0396b236adc309
Daniel Olaya <daniel.olayam@gmail.com>
2022-12-14 12:29:36 -0800

    wip. ch10

---
+++ code-ch10/dom-runner-ch10.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from importlib import reload
+from helper import run
+import network
+
+from block import GENESIS_BLOCK
+from helper import calculate_new_bits
+from network import (
+    NetworkEnvelope,
+    VersionMessage,
+)
+from io import BytesIO
+
+run(network.NetworkEnvelopeTest('test_parse'))
